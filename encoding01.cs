f7c0cd0ed932507a45f62085b939c3e5885dd06f
josefine jacobsen <josejaco@live.dk>
2020-05-25 09:39:51 +0200

    01

---
+++ encoding01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace EncodingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "jeg håber du er glad";
+            byte[] bytes = Encoding.ASCII.GetBytes(text);
+
+            foreach(byte b in bytes)
+            {
+                Console.WriteLine(b);
+            }
+            
+        }
+    }
+}
